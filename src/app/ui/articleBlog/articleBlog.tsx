425d4667b16d437bd27e7ff1e5900b1f510c0e08
Priscillia <selvapriscillia@yahoo.fr>
2024-05-17 16:15:53 +0200

    create article blog

---
+++ src/app/ui/articleBlog/articleBlog.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
+
+interface ArticleProps {
+  id: number;
+  img: string;
+  title: string;
+  subtitle: string;
+  date: string;
+  sections: { title: string; content: string }[];
+}
+
+export default function ArticleBlog({
+  id,
+  img,
+  title,
+  subtitle,
+  date,
+  sections,
+}: ArticleProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <article className="rounded overflow-hidden shadow-lg bg-white mt-4 ">
+      <div className="cursor-pointer" onClick={onOpen}>
+        <img className="w-full" src={img} alt={"image de l'article"} />
+        <h1 className="text-blue-800 font-bold text-xl mt-4 text-center">
+          {title}
+        </h1>
+        <h2 className="text-blue-700 text-base text-center">{subtitle}</h2>
+        <p className="text-gray-700 text-base ml-2 text-center mr-2">{date}</p>
+        <p className="text-gray-700 italic mt-2 text-center mb-2">
+          Cliquez pour lire l'article en entier
+        </p>
+      </div>
+      <Modal isOpen={isOpen} onOpenChange={onClose}>
+        <ModalContent>
+          {() => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">{title}</ModalHeader>
+              <ModalBody>
+                {sections.map((section) => (
+                  <section>
+                    <h2 className="text-pink-500 font-bold mb-2 mt-4 ml-2">
+                      {section.title}
+                    </h2>
+                    <p className="text-gray-600 ml-2">{section.content}</p>
+                  </section>
+                ))}
+              </ModalBody>
+                          <ModalFooter>
+                              
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Fermer
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </article>
+  );
+}
