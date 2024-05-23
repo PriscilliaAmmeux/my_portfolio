884eaf94c47efb74c257b3ffdf06bdd1653fe4f4
Priscillia <selvapriscillia@yahoo.fr>
2024-05-23 16:44:49 +0200

    change color navbar

--- src/app/ui/components/navlink/navlink.tsx
+++ src/app/ui/components/navlink/navlink.tsx
@@ -14,11 +14,12 @@ export default function NavLink({
   return (
     <Link
       href={href}
-      className={`group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-pink-500 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 ${className}`}
+      className={`group rounded-lg border border-transparent px-2 py-2 transition-colors hover:border-pink-500 hover:bg-gray-100 hover:text-pink-200 hover:pink:border-neutral-700 hover:dark:bg-neutral-800/30 ${className}`}
       target="_self"
       rel="noopener noreferrer">
       <div>
-        <h1 className={`ml-4 mb-3 text-xl font-semibold`}>
+        <h1
+          className={`ml-4 mb-3 text-xl text-pink-500 font-semibold hover:text-pink-500`}>
           {title}{" "}
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
             -&gt;
