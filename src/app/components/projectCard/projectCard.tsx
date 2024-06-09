86d451b36a80a8586649e8931594bf47901bae5f
Priscillia <selvapriscillia@yahoo.fr>
2024-06-09 21:40:37 +0200

    fix buttons projects

--- src/app/components/projectCard/projectCard.tsx
+++ src/app/components/projectCard/projectCard.tsx
@@ -22,13 +22,13 @@ export default function ProjectCard({
   githubLink,
 }: ProjectCardProps) {
   return (
-    <section className="rounded overflow-hidden shadow-lg bg-white mt-4">
+    <section className="relative rounded overflow-hidden shadow-lg bg-white mt-4">
       <img
         className="w-full"
         src={image}
         alt={"screen ou icon de l'application"}
       />
-      <section className="px-5 py-4">
+      <section className="mb-20 px-5 py-4">
         <h1 className="text-blue-800 font-bold text-xl mb-8 mt-4">{title}</h1>
         <p className="text-gray-700 text-base">{description}</p>
         <p className="text-gray-600 text-sm mt-2">{methodology}</p>
@@ -43,7 +43,7 @@ export default function ProjectCard({
           <p className="text-gray-600">{technology}</p>
         </div>
       </section>
-      <section className="px-6 py-4 flex justify-between">
+      <section className="px-6 py-4 flex justify-between absolute bottom-0 w-full">
         <Button type="button" text="Voir la démo" href={demoLink} />
         <Button type="button" text="Voir sur GitHub" href={githubLink} />
       </section>
