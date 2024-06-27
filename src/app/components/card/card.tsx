e3b7ab6efbe774277f30f949d4088cc2f932c12b
Priscillia <selvapriscillia@yahoo.fr>
2024-06-27 16:23:36 +0200

    update

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -31,7 +31,7 @@ export default function Card({
           <Icon size={40} className={styles.colorFirst} />
           <h1 className="ml-4 text-blue-800 text-2xl font-semibold">{title}</h1>
         </div>
-        <ul className="text-gray-700 list-disc ml-5">{children}</ul>
+        <div className="text-gray-700">{children}</div>
       </div>
       {button && (
         <span
--- src/app/components/itemList/itemList.tsx
+++ src/app/components/itemList/itemList.tsx
@@ -1,19 +1,9 @@
-interface IconProps extends React.SVGProps<SVGSVGElement> {
-  size?: number;
-}
+import React from "react";
 
 interface ItemListProps {
-  Icon: (props: IconProps) => JSX.Element;
   text: React.ReactNode;
 }
 
-export default function ItemList({ Icon, text }: ItemListProps) {
-  return (
-    <li className="flex items-start">
-      <div style={{ width: "20px", height: "20px" }} className="align-top">
-        <Icon className="mr-2 mt-1" />
-      </div>
-      {text}
-    </li>
-  );
+export default function ItemList({ text }: ItemListProps) {
+  return <li className="list-disc">{text}</li>;
 }
