bba0fd3bfe0fe984853ca5b1d18ad5278d96b102
Priscillia <selvapriscillia@yahoo.fr>
2024-06-27 16:57:00 +0200

    update

--- src/app/components/itemList/itemList.tsx
+++ src/app/components/itemList/itemList.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { IconType } from "react-icons";
 
 interface ItemListProps {
   text: React.ReactNode;
+  Icon?: IconType;
 }
 
-export default function ItemList({ text }: ItemListProps) {
-  return <li className="list-disc">{text}</li>;
+export default function ItemList({ text, Icon }: ItemListProps) {
+  return (
+    <li className="list-disc">
+      <span className="inline-flex items-center">
+        {Icon && <Icon className="mr-2" />}
+        {text}
+      </span>
+    </li>
+  );
 }
