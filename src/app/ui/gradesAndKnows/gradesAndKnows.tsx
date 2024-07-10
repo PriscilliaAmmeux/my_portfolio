1534e3f2fedf7da7442b5535f2c4da8bb866a885
Priscillia <selvapriscillia@yahoo.fr>
2024-07-10 22:39:15 +0200

    update knows en CV

--- src/app/ui/gradesAndKnows/gradesAndKnows.tsx
+++ src/app/ui/gradesAndKnows/gradesAndKnows.tsx
@@ -7,6 +7,8 @@ import BackendSkills from "../backendSkills/backendSkills";
 import FrontendSkills from "../frontendSkills/frontendSkills";
 import grades from "../../../data/grades.json";
 import GradesItems from "../gradesItems/gradesItems";
+import { BiHandicap } from "react-icons/bi";
+import DesignSkills from "../designSkills/designSkills";
 
 export default function GradesAndKnows() {
   return (
@@ -20,31 +22,64 @@ export default function GradesAndKnows() {
           <h1 className="flex items-center font-bold">
             Spécifier, concevoir et développer des logiciels ou applications web
           </h1>
-          <ul className="grid grid-cols-2 gap-4">
+          <ul className="grid grid-cols-3 gap-4">
             <BackendSkills />
             <FrontendSkills />
+            <DesignSkills />
           </ul>
 
-          <h2 className="flex items-center pt-3 font-bold ">
+          <h1 className="flex items-center pt-3 font-bold ">
             Concevoir, gérer et interroger des bases de données:{" "}
-          </h2>
+          </h1>
           <li className="flex items-center ml-6">
             <SiPostgresql className="ml-2 mr-2" />
             <span className="font-normal">PostgreSQL,</span>
             <SiMysql className="ml-2 mr-2" />
             <span className="font-normal">MySQL</span>
           </li>
-          <h3 className="flex items-center pt-3 font-bold">
+
+          <h1 className="flex items-center pt-3 font-bold">
             Organiser et gérer des projets informatiques en équipe:{" "}
-          </h3>
+          </h1>
           <li className="flex items-center ml-6">
             <FaGithub className="ml-2 mr-2" />{" "}
             <span className="font-normal">Git</span>
           </li>
 
-          <h4 className="flex items-center pt-3 font-bold">
+          <h1 className="flex items-center pt-3 font-bold">
             Conseiller et orienter le client dans son choix
-          </h4>
+          </h1>
+
+          <h1 className="flex items-center pt-3 font-bold">
+            Outils et technologies de déploiement:
+          </h1>
+          <li className="flex items-center ml-6">
+            <span className="font-normal">Docker, Vercel</span>
+          </li>
+
+          <h1 className="flex items-center pt-3 font-bold">Tests :</h1>
+          <li className="flex items-center ml-6">
+            JUnit, Jest, Karaté ( Framework de tests d'API REST), Playwright.
+          </li>
+
+          <h1 className="flex items-center pt-3 font-bold">
+            Accessibilité Web:
+          </h1>
+          <li className="flex items-center ml-6">
+            Techniques et pratiques d’accessibilité (formation prévue courant
+            octobre- novembre)
+          </li>
+
+          <h1 className="flex items-center pt-3 font-bold">
+            Pratiques de développement durable
+          </h1>
+          <li className="flex items-center ml-6">
+            Optimisation de la consommation énergétique des applications
+          </li>
+          <li className="flex items-center ml-6">
+            Utilisation de services et infrastructures à faible empreinte
+            carbone{" "}
+          </li>
         </ul>
       </Card>
 
