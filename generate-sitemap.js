a998641d862c643f20c8c8b9132e634eca4f2d5c
Priscillia <selvapriscillia@yahoo.fr>
2024-08-07 17:45:15 +0200

    add sitemap

---
+++ generate-sitemap.js
@@ -0,0 +1,29 @@
+var createWriteStream = require("fs").createWriteStream;
+var _a = require("sitemap"), SitemapStream = _a.SitemapStream, streamToPromise = _a.streamToPromise;
+// Define the URLs to include in the sitemap
+var urls = [
+    { url: "/", changefreq: "daily", priority: 1.0 },
+    { url: "/about", changefreq: "daily", priority: 0.9 },
+    { url: "/project", changefreq: "daily", priority: 0.8 },
+    { url: " /prestations", changefreq: "daily", priority: 0.8 },
+    { url: " /blog", changefreq: "daily", priority: 0.8 },
+    { url: " /contact", changefreq: "daily", priority: 0.7 },
+];
+// Create a sitemap stream with the specified hostname
+var sitemap = new SitemapStream({
+    hostname: "https://www.priscillia-ammeux-portfolio.fr/"
+});
+// Create a write stream for the sitemap.xml file
+var writeStream = createWriteStream("./public/sitemap.xml");
+sitemap.pipe(writeStream);
+// Write the URLs to the sitemap stream
+urls.forEach(function (url) { return sitemap.write(url); });
+// End the sitemap stream
+sitemap.end();
+// Convert the sitemap stream to a promise to handle success and errors
+streamToPromise(sitemap)
+    .then(function () {
+    console.log("Sitemap generated successfully!");
+})["catch"](function (err) {
+    console.error("Error generating sitemap:", err);
+});
---
+++ generate-sitemap.ts
@@ -0,0 +1,36 @@
+const { createWriteStream } = require("fs");
+const { SitemapStream, streamToPromise } = require("sitemap");
+
+// Define the URLs to include in the sitemap
+const urls = [
+  { url: "/", changefreq: "daily", priority: 1.0 },
+  { url: "/about", changefreq: "daily", priority: 0.9 },
+  { url: "/project", changefreq: "daily", priority: 0.8 },
+  { url: " /prestations", changefreq: "daily", priority: 0.8 },
+  { url: " /blog", changefreq: "daily", priority: 0.8 },
+  { url: " /contact", changefreq: "daily", priority: 0.7 },
+];
+
+// Create a sitemap stream with the specified hostname
+const sitemap = new SitemapStream({
+  hostname: "https://www.priscillia-ammeux-portfolio.fr/",
+});
+
+// Create a write stream for the sitemap.xml file
+const writeStream = createWriteStream("./public/sitemap.xml");
+sitemap.pipe(writeStream);
+
+// Write the URLs to the sitemap stream
+urls.forEach((url) => sitemap.write(url));
+
+// End the sitemap stream
+sitemap.end();
+
+// Convert the sitemap stream to a promise to handle success and errors
+streamToPromise(sitemap)
+  .then(() => {
+    console.log("Sitemap generated successfully!");
+  })
+  .catch((err: unknown) => {
+    console.error("Error generating sitemap:", err);
+  });
