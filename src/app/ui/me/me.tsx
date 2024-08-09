e17a0a950f4e809ef34bfdfeb3a5397da4a1df83
Priscillia <selvapriscillia@yahoo.fr>
2024-08-09 15:17:35 +0200

    update section about

--- src/app/ui/me/me.tsx
+++ src/app/ui/me/me.tsx
@@ -17,24 +17,35 @@ export default function Me() {
       <div className="ml-5 mt-4 sm:mt-0">
         <p className="mb-2">
           Bonjour ! Je suis Priscillia, conceptrice et développeuse
-          d'applications passionnée par l'accessibilité et le développement
-          écologique de vos applications.
+          d'applications, passionnée par l'accessibilité numérique et le
+          développement durable. Après une première carrière dans le secteur
+          bancaire, j'ai opéré une reconversion réussie vers le développement
+          web, portée par ma curiosité et mon envie de créer des solutions
+          numériques innovantes.
         </p>
         <p className="mb-2">
-          À la recherche de nouvelles opportunités : contrats à durée
-          déterminée, indéterminée, missions ponctuelles ou une alternance avec{" "}
-          <a
-            href="https://www.foreach-academy.fr/le-mastere-foreach/"
-            className="hover:text-pink-700 transition-colors duration-300 hover:underline hover:font-semibold"
-            target="_blank"
-            rel="noopener noreferrer">
-            l'école ForEach Academy
-          </a>{" "}
-          pour continuer à me perfectionner (RNCP Niveau 7 - Architecte Web).
+          À 37 ans, mariée et maman de deux enfants, j'ai su allier ma vie de
+          famille à une carrière en pleine expansion, en m'appuyant sur des
+          valeurs fortes comme l'honnêteté, l'inclusivité, et le respect de
+          l'environnement. Basée dans le nord de la France, j'accorde une grande
+          importance à l'organisation et à la rigueur, ce qui me permet de mener
+          à bien des projets complexes et de m'investir pleinement dans des
+          missions ambitieuses.
         </p>
         <p className="mb-2">
-          À 37 ans, mariée et maman de deux enfants, je suis basée dans le nord
-          de la France. L'organisation et la rigueur sont mes atouts.
+          Mon expertise en développement web fullstack, acquise à travers des
+          expériences variées, tant en freelance qu'en entreprise, se double
+          d'un engagement fort pour l'accessibilité et l'optimisation écologique
+          des applications. Que ce soit pour des projets personnels ou
+          professionnels, je mets un point d'honneur à créer des solutions qui
+          allient performance, accessibilité, et durabilité.
+        </p>
+        <p className="mb-2">
+          Je suis actuellement à la recherche de nouvelles opportunités où je
+          pourrai continuer à évoluer, apprendre, et contribuer à des projets
+          porteurs de sens. Mon dynamisme, ma motivation sans faille, et ma
+          capacité à communiquer efficacement font de moi une collaboratrice
+          précieuse, prête à relever tous les défis.
         </p>
       </div>
     </section>
