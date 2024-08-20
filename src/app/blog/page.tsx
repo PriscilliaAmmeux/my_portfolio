83f4b5aca0f37e429ec7abce7b2204f01e56f5e1
Priscillia <selvapriscillia@yahoo.fr>
2024-08-20 09:38:46 +0200

    display articles by dates

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,13 +10,21 @@ export const metadata: Metadata = {
 };
 
 export default function Blog() {
+  const sortedBlog = blog.sort((a, b) => {
+    const [dayA, monthA, yearA] = a.date.split("/").map(Number);
+    const [dayB, monthB, yearB] = b.date.split("/").map(Number);
+    const dateA = new Date(yearA, monthA - 1, dayA);
+    const dateB = new Date(yearB, monthB - 1, dayB);
+    return dateB.getTime() - dateA.getTime();
+  });
+
   return (
     <Layout>
       <section className="pt-4 sm:px-24">
         <Title text="Blog" icon={FaPenFancy} />
         <div className="w-full container mx-auto px-3">
           <div className="md:grid md:grid-cols-2 gap-4">
-            {blog.map((article) => (
+            {sortedBlog.map((article) => (
               <ArticleBlog
                 id={article.id}
                 key={article.id}
