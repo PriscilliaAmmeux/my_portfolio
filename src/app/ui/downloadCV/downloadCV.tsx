c17eb46348234a3a7f8a31d780f406abe25c25b2
Priscillia <selvapriscillia@yahoo.fr>
2024-09-19 14:07:03 +0200

    fix error download

--- src/app/ui/downloadCV/downloadCV.tsx
+++ src/app/ui/downloadCV/downloadCV.tsx
@@ -5,18 +5,22 @@ import Button from "@/app/components/button/button";
 export default function downloadCV() {
   return (
     <section className="flex flex-col sm:flex-row justify-around">
-      <Button
-        className="m-2 sm:m-0"
-        type={"button"}
-        text={"Télécharger mon CV"}
-        onClick={() => window.open("/CV_Priscillia_Ammeux.pdf")}
-        ariaLabel={
-          "Cliquez sur ce bouton pour télécharger mon CV pour un CDI, une mission FREELANCE ou un CDD"
-        }
-        title={
-          "Télécharger ici mon CV pour un CDI, une mission FREELANCE ou un CDD"
-        }
-      />
+      <a
+        href="/CV_Priscillia_Ammeux.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Cliquez sur ce bouton pour télécharger mon CV pour un CDI, une mission FREELANCE ou un CDD"
+        title="Télécharger ici mon CV pour un CDI, une mission FREELANCE ou un CDD"
+        className="m-2 sm:m-0">
+        <Button
+          className="w-full h-full"
+          type={"button"}
+          text={"Télécharger mon CV"}
+          ariaLabel={
+            "Cliquez sur ce bouton pour télécharger mon CV pour un CDI, une mission FREELANCE ou un CDD"
+          }
+        />
+      </a>
     </section>
   );
 }
