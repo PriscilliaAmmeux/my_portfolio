4abddb572cad8b88d7dcca0a921e2cca14e0f207
Priscillia <selvapriscillia@yahoo.fr>
2024-10-21 18:45:12 +0200

    style article impostor

--- src/app/impostorArticle/page.tsx
+++ src/app/impostorArticle/page.tsx
@@ -35,72 +35,68 @@ export default function ImpostorArticle() {
           content={
             <ul className="list-disc list-inside space-y-4">
               <li>
-                <span className="font-bold pr-1">Un peu d’histoire :</span>
+                <span className="font-bold ">Un peu d’histoire : </span>
                 Introduit en 1978 par 2 psychologues suite à une étude qu’elles
                 avaient mené sur 150 femmes diplômées, exerçant des métiers
                 prestigieux et jouissant d’une excellente réputation: Pauline
                 ROSE CLANCE et Suzanne IMES.
               </li>
               <li>
-                <span className="font-bold pr-1">
+                <span className="font-bold ">
                   Le syndrome de l'imposteur, Qu’est ce que c’est ?
                 </span>
-                <ul>
+                <ul className="space-y-4 pl-6 pt-2">
                   <li>
-                    Le syndrome de l’imposteur c’est quand tu ressens un doute
+                    - Le syndrome de l’imposteur c’est quand tu ressens un doute
                     persistant quant à ta propre valeur et à tes capacités​.
                   </li>
-                  <li>Cette impression de ne pas mériter les compliments.</li>
+                  <li>- Cette impression de ne pas mériter les compliments.</li>
                   <li>
-                    Cette croyance que si tu réussis, c’est un coup de chance ou
-                    c’est grâce à Pierre, Paul, Jacques
+                    - Cette croyance que si tu réussis, c’est un coup de chance
+                    ou c’est grâce à Pierre, Paul, Jacques
                   </li>
                   <li>
-                    Ce sentiment de ne jamais être assez doué(e), assez
+                    - Ce sentiment de ne jamais être assez doué(e), assez
                     compétent(e), assez formé(e), assez …
                   </li>
                   <li>
-                    Bref, tous ces doutes qui font que:​ tu te trouves
+                    - Bref, tous ces doutes qui font que:​ tu te trouves
                     illégitime, ​tu rejettes tes réalisations personnelles.
                   </li>
                   <li>
-                    Ce doute est nourri par la peur !​ La peur constante de mal
-                    faire et de ne pas être à la hauteur !​ La peur d’être
+                    - Ce doute est nourri par la peur !​ La peur constante de
+                    mal faire et de ne pas être à la hauteur !​ La peur d’être
                     démasqué(e)
                   </li>
                 </ul>
               </li>
               <li>
                 <span className="font-bold pr-1">Pourquoi il perdure ?</span>
-                <ul>
+                <ul className="list-decimal pl-8 space-y-4 pt-2 p">
                   <li>
-                    <span className="font-bold pr-1">
+                    <span className="font-bold ">
                       Pression de la perfection dans notre société :
-                    </span>{" "}
+                    </span>
                     Dans notre société qui valorise l'excellence, beaucoup se
                     sentent obligés d'être parfaits. Cette pression peut
                     conduire à un sentiment d'inadéquation lorsqu'on se compare
                     aux autres ou à des idéaux inatteignables.
                   </li>
                   <li>
-                    <span className="font-bold pr-1">
-                      Comparaison Sociale :
-                    </span>
+                    <span className="font-bold ">Comparaison Sociale :</span>
                     Avec les réseaux sociaux et le monde numérique, il est
                     facile de se comparer constamment aux autres, souvent sans
                     voir les défis ou les échecs qu'ils ont pu surmonter. Cela
                     renforce l'idée que l'on est moins compétent que ses pairs."
                   </li>
                   <li>
-                    <span className="font-bold pr-1">
-                      Culture de la Réussite :
-                    </span>
+                    <span className="font-bold ">Culture de la Réussite :</span>
                     Dans certains milieux, la réussite est mise sur un
                     piédestal, et toute erreur est perçue comme un échec. Cela
                     peut exacerber le sentiment de ne pas être à la hauteur.
                   </li>
                   <li>
-                    <span className="font-bold pr-1">Cycle d'Auto-Doute :</span>
+                    <span className="font-bold ">Cycle d'Auto-Doute :</span>
                     Ceux qui souffrent du syndrome de l'imposteur ont tendance à
                     minimiser leurs succès et à se concentrer sur leurs erreurs,
                     ce qui crée un cycle de doute de soi qui est difficile à
@@ -136,23 +132,21 @@ export default function ImpostorArticle() {
           content={
             <ul className="list-disc list-inside space-y-4">
               <li>
-                <span className="font-bold pr-1">Faire des listes :</span>
+                <span className="font-bold ">Faire des listes :</span>
                 Connaissances, compétences, projets réussis, reconnaissances.
               </li>
               <li>
-                <span className="font-bold pr-1">
-                  Apprendre de ses erreurs :
-                </span>
+                <span className="font-bold ">Apprendre de ses erreurs :</span>
                 Valoriser les échecs comme des opportunités d'apprentissage."
               </li>
               <li>
-                <span className="font-bold pr-1">
+                <span className="font-bold ">
                   S’entourer d’amis et de pairs:
                 </span>
                 La force du collectif.
               </li>
               <li>
-                <span className="font-bold pr-1">Partager et contribuer :</span>
+                <span className="font-bold ">Partager et contribuer :</span>
                 Parler en public, écrire des articles, contribuer à l’open
                 source.
               </li>
@@ -164,7 +158,7 @@ export default function ImpostorArticle() {
           content={
             <ul className="list-disc list-inside space-y-4">
               <li>
-                <span className="font-bold pr-1">
+                <span className="font-bold ">
                   Faire partie du comité de sélection de la DevFest Lille 2024 :
                 </span>
                 441 relecture de CFP, rencontre des personnes extraordinaires
@@ -173,14 +167,12 @@ export default function ImpostorArticle() {
                 reviewers) qui m’ont poussé à donner ce talk.
               </li>
               <li>
-                <span className="font-bold pr-1">
-                  La communauté des ChtiteDev
-                </span>
+                <span className="font-bold ">La communauté des ChtiteDev</span>
                 qui m’a encouragé à faire ce talk
               </li>
               <li>
-                <span className="font-bold pr-1">Oser : </span>aujourd’hui je
-                fais partie du collectif KreaWebSolutions, un collectif de 5
+                <span className="font-bold ">Oser : </span>aujourd’hui je fais
+                partie du collectif KreaWebSolutions, un collectif de 5
                 personnes car j’ai osé contacter Arnaud sur LinkedIn.
               </li>
             </ul>
@@ -191,13 +183,13 @@ export default function ImpostorArticle() {
           content={
             <ul className="list-disc list-inside space-y-4">
               <li>
-                <span className="font-bold pr-1">
+                <span className="font-bold ">
                   Accepter ses Limites et Éviter le Perfectionnisme :
                 </span>
                 On ne peut pas tout savoir, n’ayez pas peur de l’échec.
               </li>
               <li>
-                <span className="font-bold pr-1">
+                <span className="font-bold ">
                   Les Refus ne sont pas des Échecs :
                 </span>
                 les refus d'emplois ou d'opportunités ne sont pas des échecs,
