28069314df3ac19f610bd479524772f339530b90
Priscillia <selvapriscillia@yahoo.fr>
2025-07-10 14:30:31 +0200

    add promo summer 2025 and update logo

--- src/app/ui/navbar/navbar.tsx
+++ src/app/ui/navbar/navbar.tsx
@@ -4,7 +4,7 @@ import Logo from "../logo/logo";
 export default function Navbar() {
   return (
     <section className="bg-pink-100 dark:bg-gray-700 w-full grid lg:max-w-none lg:w-full lg:mb-0 lg:grid-cols-7 lg:text-center items-center">
-      <Logo width={100} height={100} />
+      <Logo width={125} height={125} />
       <NavLink href="/" title="Accueil" isActive={true} />
       <NavLink href="/project" title="Mes projets" isActive={true} />
       <NavLink href="/prestations" title="Prestations" isActive={true} />
