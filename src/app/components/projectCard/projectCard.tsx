6b87cf7e8d9284dab90f742b2242419931316706
Priscillia <selvapriscillia@yahoo.fr>
2025-07-21 22:56:00 +0200

    update page projects

--- src/app/components/projectCard/projectCard.tsx
+++ src/app/components/projectCard/projectCard.tsx
@@ -4,28 +4,18 @@ import Image from "next/image";
 type ProjectCardProps = {
   title: string;
   description: string;
-  methodology: string;
-  how: string;
   image: string;
-  tools: string;
-  technology: string;
   demoLink: string;
-  githubLink: string;
 };
 
 export default function ProjectCard({
   title,
   description,
-  methodology,
-  how,
   image,
-  tools,
-  technology,
   demoLink,
-  githubLink,
 }: ProjectCardProps) {
   return (
-    <section className="relative rounded overflow-hidden shadow-lg bg-white mt-4 ">
+    <section className="relative overflow-hidden shadow-lg bg-pink-700 mt-4 ">
       <Image
         className="w-full"
         src={image}
@@ -33,35 +23,18 @@ export default function ProjectCard({
         height={200}
         alt={"screen ou icon de l'application"}
       />
-      <section className="mb-20 px-5 py-4">
-        <h2 className="text-blue-800 font-bold text-xl mb-8 mt-4">{title}</h2>
-        <p className="text-gray-700 text-base text-justify">{description}</p>
-        <p className="text-gray-600 text-sm mt-2">{methodology}</p>
-        <p className="text-gray-600 text-sm mt-2">{how}</p>
-        <div className="mt-4">
-          <p className="text-gray-700 font-semibold ">Outils utilisées :</p>
-          <p className="text-gray-600 text-justify">{tools}</p>
-        </div>
-        <div className="mt-4">
-          <p className="text-gray-700 font-semibold">
-            Technologies utilisées :
-          </p>
-          <p className="text-gray-600 text-justify">{technology}</p>
-        </div>
+      <section className="mb-20 px-5 py-4 text-white">
+        <h2 className="text-white font-bold text-xl mb-4 ">{title}</h2>
+        <p className="text-base text-justify">{description}</p>
       </section>
-      <section className="px-6 py-4 flex justify-between absolute bottom-0 w-full">
+      <section className="px-6 py-4 flex justify-center absolute bottom-0 w-full">
         <Button
           type="button"
           text="Visiter le site web"
           href={demoLink}
           ariaLabel="Cliquez sur le bouton pour visiter le site web"
-          className="mr-2"
-        />
-        <Button
-          type="button"
-          text="Voir sur GitHub"
-          href={githubLink}
-          ariaLabel="Cliquez sur le bouton pour visiter le Github du site"
+          className="mr-2 "
+          variant="bgWhite"
         />
       </section>
     </section>
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,7 +1,6 @@
 import ProjectCard from "../components/projectCard/projectCard";
 import projects from "../../data/projects.json";
 import Title from "../ui/title/title";
-import { MdComputer } from "react-icons/md";
 import { Metadata } from "next";
 import Layout from "../ui/layout/layout";
 
@@ -21,21 +20,16 @@ export const metadata: Metadata = {
 export default function Project() {
   return (
     <Layout>
-      <section className="pt-4 sm:px-24">
-        <Title text="Mes projets" icon={MdComputer} />
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 container mx-auto mx-4 justify-items-stretch">
+      <section className="pt-4 pb-10 sm:px-24">
+        <Title text="Mes projets" />
+        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4 container mx-auto mx-4 justify-items-stretch">
           {projects.map((project) => (
             <ProjectCard
               key={project.id}
               title={project.title}
               description={project.description}
-              methodology={project.methodology}
-              how={project.how}
               image={project.image}
-              tools={project.tools}
-              technology={project.technology}
               demoLink={project.demoLink}
-              githubLink={project.githubLink}
             />
           ))}
         </div>
